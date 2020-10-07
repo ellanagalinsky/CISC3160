49b00e5df4d108b41aefae09d207d09d9a7aefdc
ellanagalinsky <ellanagalinsky@yahoo.com>
2020-10-07 13:11:25 -0400

    lab3

---
+++ Assignments/Lab_3/FibonacciNum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+/* PSEUDOCODE:
+    int x, y
+    while loop for numbers less than 4,000,000
+    if sum1 of x + y is evem
+        then y value is added to sum2
+    y value is assigned to x
+    sum1 value is assigned to y to proceed to the next summation in the sequence 
+*/
+
+int main(){
+    int sum1, sum2, x, y;
+
+    while (y <= 4000000){
+        sum1 = x + y;
+        if (sum1 % 2 == 0){
+            sum2 += y;
+        }
+        x = y;
+        y = sum1;
+    }
+
+    cout << sum2;
+
+}
---
+++ Assignments/Lab_3/README.txt
@@ -0,0 +1,11 @@
+The two files in this repository contain the codes for the Fibonacci Numbers
+I created a while loop for the c++ file, and for the python file.
+
+I used the c++ documentation as a guide:
+https://devdocs.io/cpp/
+
+In addition, I used python documentation as a guide for the python file:
+https://docs.python.org/3/
+
+It was useful and more efficient using sum() in python versus declaring a separate
+variable for sum and then using operands
